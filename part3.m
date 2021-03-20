e28b38a627e908c4348393664c00f09ed6ab301d
Eda Albert <Albert@Edas-MacBook-Pro.local>
2021-03-19 23:34:08 -0400

    first commit

---
+++ part3.m
@@ -0,0 +1,67 @@
+clc
+clear
+set(0,'DefaultFigureWindowStyle','docked')
+
+
+% This example shows how to calculate and plot both the
+% fundamental TE and TM eigenmodes of an example 3-layer ridge
+% waveguide using the full-vector eigenmode solver.  
+
+
+% Name: Eda Oritseserundede 
+% CUID: 100993421
+
+% Refractive indices:
+n1 = 3.34;          % Lower cladding
+n2 = 3.44;          % Core
+n3 = 1.00;          % Upper cladding (air)
+
+% Layer heights:
+h1 = 2.0;           % Lower cladding
+h2 = 1.3;           % Core thickness
+h3 = 0.5;           % Upper cladding
+
+% Horizontal dimensions:
+rh = 1.1;           % Ridge height
+rw = 1.0;           % Ridge half-width
+side = 1.5;         % Space on side
+
+% Grid size:
+dx = 0.0125;        % grid size (horizontal)
+dy = 0.0125;        % grid size (vertical)
+
+lambda = 1.55;      % vacuum wavelength
+nmodes = 1;         % number of modes to compute
+
+counter = 0; 
+sum_neff = zeros(10,1);
+% For loop 
+for n2 = linspace(3.305,3.44,10)
+    counter = counter + 1;
+[x,y,xc,yc,nx,ny,eps,edges] = waveguidemesh([n1,n2,n3],[h1,h2,h3], ...
+                                            rh,rw,side,dx,dy); 
+
+% First consider the fundamental TE mode:
+
+[Hx,Hy,neff] = wgmodes(lambda,n2,nmodes,dx,dy,eps,'000A');
+sum_neff(counter) = sum_neff; 
+fprintf(1,'neff = %.6f\n',neff);
+
+figure(1);
+subplot(121);
+contourmode(x,y,Hx);
+title('Hx (TE mode)'); xlabel('x'); ylabel('y'); 
+for v = edges, line(v{:}); end
+
+subplot(122);
+contourmode(x,y,Hy);
+title('Hy (TE mode)'); xlabel('x'); ylabel('y'); 
+for v = edges, line(v{:}); end
+
+figure(counter + 1);
+plot(linspace(3.305,3.44,10), neff_sum);
+xlabel('Core eff');
+ylabel('n_eff');
+
+
+end 
